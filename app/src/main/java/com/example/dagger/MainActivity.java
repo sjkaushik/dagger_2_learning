aeead20db3160cdf792f9dce0552f271003c0850
kaushik <kaushik.s@recro.io>
2019-11-03 17:15:52 +0530

    initial commit_1

--- app/src/main/java/com/example/dagger/MainActivity.java
+++ app/src/main/java/com/example/dagger/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.dagger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private  static String TAG =  "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "onCreate: ");
     }
 }
